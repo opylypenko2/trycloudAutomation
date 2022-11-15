916365695842b815e2be4cd2870ebe721c4b6bd6
opylypenko2 <106551323+opylypenko2@users.noreply.github.com>
2022-11-14 22:07:17 -0500

    Update CukesRunner.java

--- src/test/java/com/trycloud/runner/CukesRunner.java
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html",
+        plugin = {"json:target/cucumber.json", "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt", "me.jvt.cucumber.report.PrettyReports:target/cucumber"},
         features = "src/test/resources/features",
         glue = "com/trycloud/step_definitions",
